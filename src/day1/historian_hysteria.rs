f1296a7e58ad66e32684450349609850a8327cc7
Sören Reichardt <soren.reichardt@neo4j.com>
2024-12-01 12:52:29 +0100

    Solve day1 part2

--- src/day1/historian_hysteria.rs
+++ src/day1/historian_hysteria.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn pair_distances(input: &str) -> u32 {
     let (mut left, mut right) = input
         .lines()
@@ -15,3 +17,18 @@ pub fn pair_distances(input: &str) -> u32 {
     
     left.iter().zip(right.iter()).map(|(lhs, rhs)| lhs.abs_diff(*rhs)).sum()
 } 
+
+pub fn similarity_score(input: &str) -> u32 {
+    let (left, right) = input
+        .lines()
+        .map(|line| line.split_whitespace().collect::<Vec<_>>())
+        .map(|parts| (parts[0].parse::<u32>().unwrap(), parts[1].parse::<u32>().unwrap()))
+        .fold((Vec::new(), HashMap::new()), |(mut left_vec, mut right_vec), (lhs, rhs)| {
+            left_vec.push(lhs);
+            *right_vec.entry(rhs).or_insert(0u32) += 1;
+
+            (left_vec, right_vec)
+        });
+    
+    left.iter().map(|num| right.get(num).unwrap_or(&0) * num).sum()
+}
--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -4,3 +4,8 @@ pub fn part1() {
     let input = std::str::from_utf8(include_bytes!("../../res/day1/part1")).unwrap();;
     println!("{}", historian_hysteria::pair_distances(input));
 }
+
+pub fn part2() {
+    let input = std::str::from_utf8(include_bytes!("../../res/day1/part1")).unwrap();;
+    println!("{}", historian_hysteria::similarity_score(input));
+}
