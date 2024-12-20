e5286be33a12365abd9dc1e655d176ea108646c4
Sören Reichardt <soren.reichardt@neo4j.com>
2024-12-20 13:52:03 +0100

    Refactor to analytical solution

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -7,20 +7,28 @@ use winnow::Parser;
 
 pub fn part1() {
     let mut input = include_str!("../../res/day13/part1");
-    println!("{}", cheapest_wins(&mut input));
+    println!("{}", cheapest_wins(&mut input, false));
 }
 
-fn cheapest_wins(input: &mut &str) -> usize {
+pub fn part2() {
+    let mut input = include_str!("../../res/day13/part1");
+    println!("{}", cheapest_wins(&mut input, true));
+}
+
+
+fn cheapest_wins(input: &mut &str, scaled: bool) -> i64 {
     input.split("\n\n")
-        .map(|mut group| ClawMachine::from(&mut group).cheapest_win())
+        .map(|mut group| ClawMachine::from(&mut group).cheapest_win(scaled))
         .sum()
 }
 
 #[derive(Clone, Debug, PartialEq)]
 struct Position {
-    x: usize,
-    y: usize,
+    x: i64,
+    y: i64,
 }
+
+struct Line(Position, Position);
 
 struct ClawMachine {
     button_a: Position,
@@ -28,10 +36,10 @@ struct ClawMachine {
     prize: Position,
 }
 
-impl std::ops::Mul<usize> for Position {
+impl std::ops::Mul<i64> for Position {
     type Output = Self;
 
-    fn mul(self, value: usize) -> Self::Output {
+    fn mul(self, value: i64) -> Self::Output {
         Position {
             x: self.x * value,
             y: self.y * value,
@@ -61,18 +69,44 @@ impl std::ops::Sub for Position {
     }
 }
 
+impl Line {
+    fn intersection(&self, other: &Self) -> Option<Position> {
+        let a1 = self.1.y - self.0.y;
+        let b1 = self.0.x - self.1.x;
+        let c1 = a1 * self.0.x + b1 * self.0.y;
+
+        let a2 = other.1.y - other.0.y;
+        let b2 = other.0.x - other.1.x;
+        let c2 = a2 * other.0.x + b2 * other.0.y;
+
+        let delta = a1 * b2 - a2 * b1;
+
+        if delta == 0 {
+            return None;
+        }
+
+        let intersection_x = (b2 * c1 - b1 * c2) / delta;
+        let intersection_y = (a1 * c2 - a2 * c1) / delta;
+
+        Some(Position {
+            x: intersection_x,
+            y: intersection_y,
+        })
+    }
+}
+
 impl From<&mut &str> for ClawMachine {
     fn from(input: &mut &str) -> Self {
         fn parse_button(input: &mut &str, button_name: &str) -> PResult<Position> {
             preceded(format!("Button {}: ", button_name).as_str(), separated_pair(preceded("X+", digit1), ", ", preceded("Y+", digit1)))
                 .parse_next(input)
-                .map(|(x, y)| Position { x: x.parse::<usize>().unwrap(), y: y.parse::<usize>().unwrap() })
+                .map(|(x, y)| Position { x: x.parse::<i64>().unwrap(), y: y.parse::<i64>().unwrap() })
         }
 
         fn parse_prize(input: &mut &str) -> PResult<Position> {
             preceded("Prize: ", separated_pair(preceded("X=", digit1), ", ", preceded("Y=", digit1)))
                 .parse_next(input)
-                .map(|(x, y)| Position { x: x.parse::<usize>().unwrap(), y: y.parse::<usize>().unwrap() })
+                .map(|(x, y)| Position { x: x.parse::<i64>().unwrap(), y: y.parse::<i64>().unwrap() })
         }
 
         let button_a = parse_button(input, "A").unwrap();
@@ -86,27 +120,24 @@ impl From<&mut &str> for ClawMachine {
 }
 
 impl ClawMachine {
-    fn cheapest_win(&self) -> usize {
-        let Position { x: prize_x, y: prize_y} = self.prize;
+    fn cheapest_win(&self, scaled: bool) -> i64 {
+        let prize = if scaled { self.prize.clone() * 10000000000000 } else { self.prize.clone() };
 
-        let min_multiple_a = prize_x.div_ceil(self.button_a.x).min(prize_y.div_ceil(self.button_a.y));
+        let line_a = Line(Position { x: 0, y: 0 }, self.button_a.clone());
+        let line_b = Line(prize.clone() - self.button_b.clone(), prize.clone());
 
-        let mut a_multiple = min_multiple_a;
-        let mut b_multiple = 0usize;
+        line_a.intersection(&line_b)
+            .map(|intersection_point| {
+                let multiplier_a = intersection_point.x / self.button_a.x;
+                let multiplier_b = (prize.x - multiplier_a * self.button_a.x) / self.button_b.x;
 
-        while a_multiple > 0 {
-            let claw_position = self.button_a.clone() * a_multiple + self.button_b.clone() * b_multiple;
-            if claw_position == self.prize {
-                return 3 * a_multiple + b_multiple;
-            }
-            if claw_position.x > self.prize.x && claw_position.y > self.prize.y {
-                a_multiple -= 1;
-            } else {
-                b_multiple += 1;
-            }
-        }
-
-        0
+                if self.button_a.clone() * multiplier_a +  self.button_b.clone() * multiplier_b != prize {
+                    0
+                } else {
+                    3 * multiplier_a + multiplier_b
+                }
+            })
+            .unwrap_or(0)
     }
 }
 
@@ -131,6 +162,26 @@ Prize: X=7870, Y=6450
 Button A: X+69, Y+23
 Button B: X+27, Y+71
 Prize: X=18641, Y=10279";
-        assert_eq!(480, cheapest_wins(&mut input));
+        assert_eq!(480, cheapest_wins(&mut input, false));
+    }
+
+    #[test]
+    fn part2() {
+        let mut input = "Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279";
+        assert_eq!(480, cheapest_wins(&mut input, true));
     }
 }
